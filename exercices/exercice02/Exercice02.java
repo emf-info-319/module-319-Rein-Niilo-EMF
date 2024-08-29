e955b1cc0be4f3648dba6b438a64c9307d7223b0
Rein-Niilo-EMF <Niilo.Rein@studentfr.ch>
2024-08-29 11:46:26 +0200

    exercices

---
+++ exercices/exercice02/Exercice02.java
@@ -0,0 +1,13 @@
+
+
+public class Exercice02 {
+    
+    public static void main(String[] args) {
+        System.out.println("Bonjour tout le monde!");
+
+        System.out.println("mane cucushi");
+
+
+}
+    
+}
---
+++ exercices/exercice03/TypesDeBase.java
@@ -0,0 +1,10 @@
+public class TypesDeBase {
+    public static void main(String[] args) {
+
+        String nom = "Niilo" ;
+        int age = 16;
+        System.out.println("je m'appelle " + nom + " et j'ai" +" "+age);
+        System.out.println("dans 10 ans, j'aurais" + (age+10));
+    }
+    
+}
