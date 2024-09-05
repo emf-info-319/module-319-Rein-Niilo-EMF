347897fc303334b992c6d0847015895e9bd4bb6e
Rein-Niilo-EMF <Niilo.Rein@studentfr.ch>
2024-09-05 11:44:15 +0200

    Modif exercices

---
+++ exercices/exercice05/ExerciceCondition2.java
@@ -0,0 +1,12 @@
+public class ExerciceCondition2 {
+    public static void main(String[] arg){
+        int valeurATester = 1;
+        
+        if (valeurATester % 2 == 0){
+            System.out.println("la valeur est pair");
+        } else {
+            System.out.println("la valeur est impair");
+        }
+        
+    }
+}
---
+++ exercices/exercice05/ExerciceCondition3.java
@@ -0,0 +1,16 @@
+public class ExerciceCondition3 {
+    public static void main(String[] arg){
+        int Variable1 = 1;
+        int Variable2 = 6;
+
+        if (Variable1 % 2 == 0){
+            System.out.println("le produit sera pair");
+        } else {
+            if (Variable2 % 2 == 0){
+                System.out.println("le produit sera pair");
+            } else {
+                System.out.println("le produit sera impair");
+            }
+        }
+    }
+}
