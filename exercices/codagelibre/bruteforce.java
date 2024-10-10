f2975acc3fdd3ad2ea4eda0cb3f8c6828276d82c
Rein-Niilo-EMF <Niilo.Rein@studentfr.ch>
2024-10-10 10:48:47 +0200

    idk

--- exercices/codagelibre/bruteforce.java
+++ exercices/codagelibre/bruteforce.java
@@ -4,7 +4,9 @@ import java.util.Base64;
 
 public class bruteforce {
     public static final String PASSWORD = "bTBsZHU=";
-    public static final char[] CHARACTERS = new char[]{'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+    public static final char[] CHARACTERS = new char[] { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b',
+            'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w',
+            'x', 'y', 'z' };
     public static final int MAXCHAR = 5;
 
     public static void main(String[] args) {
@@ -12,19 +14,29 @@ public class bruteforce {
         /*
          * à faire : afficher le mot de passe trouvé avec un sout
          * Le mot de passe contient exactement entre 0 et 5 caractères
-         * Pour tester si un mot de passe est correct, il faut appeler la méthode testPassword en lui donnant en paramètre le password à tester
+         * Pour tester si un mot de passe est correct, il faut appeler la méthode
+         * testPassword en lui donnant en paramètre le password à tester
          */
-        int char_amount;
-        int char_rotation;
-        int charplace_rotation = 0;
-        char[] Passwordarray;
-        Passwordarray = new char[]{};
-        for (int i = 0; i < CHARACTERS.length ; i++){
-            Passwordarray[charplace_rotation] = CHARACTERS[i];
+        int char_amount = 1;
+        int char_place_rotation = 0;
+        char[] passwordarray;
+        passwordarray = new char[] {};
+        String test = "";
+        for (;;) {
+            char_amount++;
+            for (int j = 0; j < char_amount; j++) {
+                for (int i = 0; i < CHARACTERS.length; i++) {
+                    passwordarray[char_place_rotation] = CHARACTERS[i];
+                }
+                char_place_rotation++;
+            }
+            test = passwordarray[0];
+            for (int k = 1; k < passwordarray.length; k++)
+                test = test + "" + passwordarray[k];
+            System.out.println(test);
         }
 
     }
-
 
     public static boolean testPassword(String password) {
         boolean retour = false;
