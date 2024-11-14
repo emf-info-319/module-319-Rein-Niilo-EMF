0ee848641f7ef440ad57ff370b85bf3eb815da29
Rein-Niilo-EMF <Niilo.Rein@studentfr.ch>
2024-11-14 08:22:09 +0100

    idk

---
+++ pourRP.java
@@ -0,0 +1,12 @@
+public class pourRP {
+    public static int variable = nombre; // remplacez nombre par le nombre souhaité
+    public static void main(String[] args) {
+        // ceci est l’endroit où vous allez écrire votre code
+
+        int a = 5;
+int b = 3;
+int resultat = a ^ b ;
+
+    }
+        
+}
