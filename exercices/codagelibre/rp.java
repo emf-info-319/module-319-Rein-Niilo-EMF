59ae63dd21e140bd8543520f0199fa9771259667
Rein-Niilo-EMF <Niilo.Rein@studentfr.ch>
2024-11-14 13:21:46 +0100

    idk

---
+++ exercices/codagelibre/rp.java
@@ -0,0 +1,18 @@
+package exercices.codagelibre;
+
+public class rp {
+    public static void main(String[] args) {
+        if (condition){// remplacez condition par votre condition souhaité par exemple 1 == 1
+            //Action
+        } else if (condition2) {
+            // autre action
+        }
+        int[] monTableau ;
+        monTableau[2] = 3;
+    }
+    public static void methode(){
+
+    }
+
+
+}
